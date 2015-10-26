e6433f2ff82ae96aad3d8106031d9173ee03f22f
Oleksii Chernenko <oleksiich@ee.playtech.corp>
2015-10-26 17:23:13 +0200

    Mod controller with pretty logic

--- src/main/java/com/WSschool/Controller/SimpleController.java
+++ src/main/java/com/WSschool/Controller/SimpleController.java
@@ -1,7 +1,23 @@
 package com.WSschool.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by oleksiich on 10/26/2015.
  */
+@RestController
 public class SimpleController {
+
+    @RequestMapping(method=RequestMethod.GET, name = "/test")
+    public String test(@RequestParam(name = "id",required = true ) Integer id)
+    {
+        if(id<100){
+           return "hello " + id;
+    }
+        return "none";
+    }
+
 }
